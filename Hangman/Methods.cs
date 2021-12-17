76c38d63ce9d4fe148ea09136277db23d4c6f440
SmolSwede <samuel_schon@hotmail.com>
2021-12-17 11:48:40 +0100

    Add project files.

---
+++ Hangman/Methods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace Hangman
+{
+    public class Methods
+    {
+        private string correctWord;
+        
+        public char[] GetWordFromList()
+        {
+            List<string> wordList = new List<string>()
+            {
+                "lego", "duplo", "hotwheels"
+            };
+
+            int rndNum = new Random().Next(0, wordList.Count);
+            correctWord = wordList[rndNum];
+
+            return wordList[rndNum].ToCharArray();
+        }
+
+        public bool GuessLetter(string userInput, string alreadyGuessed, out string newUserInput)
+        {
+            while (alreadyGuessed.Contains(userInput))
+            {
+                Clear();
+                WriteLine("This letter has already been guessed enter a new one.");
+                userInput = ReadLine().Trim().ToLower();
+            }
+            newUserInput = userInput;
+            if (correctWord.Contains(userInput))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool GuessWord(string userInput)
+        {
+            if (userInput.Equals(correctWord))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Hangman/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+
+namespace Hangman
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+
+            int wrongCounter = 10;
+            string guessedLetters = "";
+            char[] correctLetters = methods.GetWordFromList();
+            char[] wordToReveal = new char[correctLetters.Length];
+            wordToReveal = new StringBuilder().Append('-', correctLetters.Length).ToString().Trim().ToCharArray();
+
+            WriteLine("Welcome to the Hangman Game!" +
+                      "\n" +
+                      "\nYou have 10 tries to guess the correct word." +
+                      "\n");
+            for(int i = 0; i < (10 + correctLetters.Length); i++)
+            {
+                if(wrongCounter == 0)
+                {
+                    break;
+                }
+
+                WriteLine($"The word is {string.Concat(wordToReveal)}" +
+                          $"\nGuessed letters [{guessedLetters}]" +
+                          $"\nYour amount of guesses left are {wrongCounter}");
+
+                string userInput = ReadLine().Trim().ToLower();
+
+                if(userInput.Length > 1)
+                {
+                    Clear();
+                    if(methods.GuessWord(userInput) == true)
+                    {
+                        WriteLine($"You guessed correctly the word was {string.Concat(correctLetters)}");
+                        ReadKey();
+                        break;
+                    }
+                    else
+                    {
+                        wrongCounter--;
+                        WriteLine("Your guess was wrong");
+                        ReadKey();
+                    }
+                }
+                else if(userInput.Length <= 1)
+                {
+                    Clear();
+                    if (methods.GuessLetter(userInput, guessedLetters, out userInput) == true)
+                    {
+                        guessedLetters = guessedLetters + new StringBuilder().Append(userInput + ".");
+                        for(int j = 0; j < correctLetters.Length; j++)
+                        {
+                            if (userInput.Contains(correctLetters[j]))
+                            {
+                                wordToReveal[j] = correctLetters[j];
+                            }
+                        }
+                        WriteLine($"Your guess was correct [{string.Concat(wordToReveal)}]");
+                        ReadKey();
+                    }
+                    else
+                    {
+                        wrongCounter--;
+                        WriteLine("Your guess was wrong");
+                        ReadKey();
+                    }
+                }
+                
+            }
+        }
+    }
+}
+            
